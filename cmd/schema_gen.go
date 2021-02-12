120da9d7ad2f4a9a9cc2f0edbb33b17162ec694d
Rob Knight <robknight@monzo.com>
2021-02-12 12:07:13 +0000

    fix mistake in name of schema-gen command

--- cmd/schema_gen.go
+++ cmd/schema_gen.go
@@ -18,7 +18,7 @@ func init() {
 }
 
 var schemaGenCmd = &cobra.Command{
-	Use:               "schema_gen [model name]",
+	Use:               "schema-gen [model name]",
 	Short:             "Generates the YML schema file for a given model",
 	Args:              cobra.ExactValidArgs(1),
 	ValidArgsFunction: completeModelFn,
