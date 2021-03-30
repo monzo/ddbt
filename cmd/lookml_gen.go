c9bdd0f1143e7147b39c2f8591c773aabf05d79d
Akhat Rakishev <akharakishev@monzo.com>
2021-03-30 12:31:00 +0600

    add more error handling and changed formatting

--- cmd/lookml_gen.go
+++ cmd/lookml_gen.go
@@ -3,29 +3,30 @@ package cmd
 import (
 	"ddbt/bigquery"
 	"ddbt/config"
+	"errors"
 
 	"fmt"
 	"os"
 
-	"github.com/spf13/cobra"
 	"github.com/atotto/clipboard"
+	"github.com/spf13/cobra"
 )
 
 // map bigquery data types to looker data types
 var mapBqToLookerDtypes map[string]string = map[string]string{
-	    "INTEGER": "number",
-	    "FLOAT":   "number",
-	    "NUMERIC": "number",
-	    "BOOLEAN": "yesno",
-	    "STRING": "string",
-	    "TIMESTAMP": "time",
-	    "DATETIME": "time",
-	    "DATE":  "time",
-	    "TIME": "time",
-	    "BOOL": "yesno",
-	    "ARRAY": "string",
-	    "GEOGRAPHY": "string",
-	}
+	"INTEGER":   "number",
+	"FLOAT":     "number",
+	"NUMERIC":   "number",
+	"BOOLEAN":   "yesno",
+	"STRING":    "string",
+	"TIMESTAMP": "time",
+	"DATETIME":  "time",
+	"DATE":      "time",
+	"TIME":      "time",
+	"BOOL":      "yesno",
+	"ARRAY":     "string",
+	"GEOGRAPHY": "string",
+}
 
 // specify looker timeframes for datetime/date/time variable data types
 const timeBlock string = `timeframes: [
@@ -76,38 +77,39 @@ var lookmlGenCmd = &cobra.Command{
 func getColumnsForModelWithDtypes(modelName string, target *config.Target) (columns []string, dtypes []string, err error) {
 	schema, err := bigquery.GetColumnsFromTable(modelName, target)
 	if err != nil {
-		return nil, nil, err
-	}
-
-	// itereate over fields, record field names and data types
-	for _, fieldSchema := range schema {
-		column := fmt.Sprintf("%v", fieldSchema.Name)
-		dtype := fmt.Sprintf("%v", fieldSchema.Type)
-		columns = append(columns, column)
-		dtypes = append(dtypes, dtype)
-
-	}
-	return columns, dtypes, nil
-}
-
-func generateNewLookmlView(modelName string, target *config.Target) (error) {
-	bqColumns, bqDtypes, err := getColumnsForModelWithDtypes(modelName, target)
-	if err != nil {
 		fmt.Println("Could not retrieve schema from BigQuery")
 		os.Exit(1)
 	}
 
+	// itereate over fields, record field names and data types
+	for _, fieldSchema := range schema {
+		columns = append(columns, fieldSchema.Name)
+		dtypes = append(dtypes, string(fieldSchema.Type))
+	}
+	return columns, dtypes, err
+}
+
+func generateNewLookmlView(modelName string, target *config.Target) error {
+	bqColumns, bqDtypes, err := getColumnsForModelWithDtypes(modelName, target)
+	if err != nil {
+		fmt.Println("Retrieved BigQuery schema but failed to parse it")
+		os.Exit(1)
+	}
+
 	// initialise lookml view head
-	lookmlView := "view: " + modelName +" {\n\n"
+	lookmlView := "view: " + modelName + " {\n\n"
 	lookmlView += "sql_table_name: `" + target.ProjectID + "." + target.DataSet + "." + modelName + "` ;;\n"
 
 	// add dimensions and appropriate blocks for each field
-	for i := 0; i < len(bqColumns); i ++ {
+	for i := 0; i < len(bqColumns); i++ {
 		colName := bqColumns[i]
 		colDtype := mapBqToLookerDtypes[bqDtypes[i]]
+		if colDtype == "" {
+			return errors.New("Did not find Looker data type corresponding to BigQuery data type: " + bqDtypes[i])
+		}
 		newBlock := "\n"
-		
-		if colDtype == "date_time" || colDtype == "date" || colDtype == "time"{
+
+		if colDtype == "date_time" || colDtype == "date" || colDtype == "time" {
 			newBlock += "dimension_group: " + colName + " {\n"
 			newBlock += "type: " + colDtype + "\n"
 			newBlock += timeBlock
@@ -115,7 +117,7 @@ func generateNewLookmlView(modelName string, target *config.Target) (error) {
 			newBlock += "dimension: " + colName + " {\n"
 			newBlock += "type: " + colDtype + "\n"
 		}
-		
+
 		newBlock += "sql: ${TABLE}." + colName + " ;;\n}\n"
 
 		lookmlView += newBlock
@@ -124,9 +126,12 @@ func generateNewLookmlView(modelName string, target *config.Target) (error) {
 	// add closing curly bracket and copy to clipboard
 	lookmlView += "}"
 
-	clipboard.WriteAll(lookmlView)
+	err = clipboard.WriteAll(lookmlView)
+	if err != nil {
+		fmt.Println("Could not write generated LookML to your clipboard")
+		os.Exit(1)
+	}
 	fmt.Println("\n✅ LookML view for " + modelName + " has been copied to your clipboard!")
 
 	return nil
 }
-
