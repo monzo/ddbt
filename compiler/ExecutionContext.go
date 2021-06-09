89d4c4a82f376b8f4117c225d9b0bbd1aec743fe
borjavazquez <borjavazquez@monzo.com>
2021-06-09 14:54:31 +0100

    Update ExecutionContext.go

--- compiler/ExecutionContext.go
+++ compiler/ExecutionContext.go
@@ -119,7 +119,7 @@ func (e *ExecutionContext) RegisterUpstreamAndGetRef(modelName string, fileType 
 	}
 
 	switch upstream.GetMaterialization() {
-	case "table", "incremental", "project_sharded_table":
+	case "table", "incremental", "project_sharded_table", "view":
 		// If "--upstream=target" has been provided and this model is not in the DAG, then we read from the upstream
 		// target, rather than the target defined in "--target=target"
 		if target.ReadUpstream != nil && !upstream.IsInDAG() {
