6c79836aa994f874d7b3eaf8f9ad3756577499ec
twindebank <theowindebank@monzo.com>
2021-08-13 15:08:12 +0100

    ddbt isolate-dag: warn on empty DAG instead of exit (#45)
    * warn on empty dag instead of exit
    
    * use fail-on-not-found flag

--- cmd/run.go
+++ cmd/run.go
@@ -299,8 +299,13 @@ func buildGraph(fileSystem *fs.FileSystem, modelFilters []string) *fs.Graph {
 	pb.Increment()
 
 	if graph.Len() == 0 {
-		fmt.Printf("❌ Empty DAG generated for model filter: %s\n", strings.Join(modelFilters, ", "))
-		os.Exit(1)
+		switch FailOnNotFound {
+		case true:
+			fmt.Printf("❌ Empty DAG generated for model filter: %s\n", strings.Join(modelFilters, ", "))
+			os.Exit(1)
+		case false:
+			fmt.Printf("⚠️ Empty DAG generated for model filter: %s\n", strings.Join(modelFilters, ", "))
+		}
 	}
 
 	return graph
